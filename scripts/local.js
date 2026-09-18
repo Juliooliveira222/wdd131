9089ce43f739785f7571df02692b7e78b2c64fec
Julio Oliveira Batista de Jesus <julinhooliveira222@gmail.com>
2026-09-18 00:59:46 +0000

    atualização Js

--- scripts/local.js
+++ scripts/local.js
@@ -1,12 +1,15 @@
-
+// 1. Inserir Ano Atual
 document.getElementById("ano").textContent = new Date().getFullYear();
-document.getElementById("modificacao").textContent = document.lastModified;
 
-const temp = 9;   // Temperatura em °C
-const vento = 10; // Vento em km/h
+// 2. Inserir Última Modificação formatada
+const dataMod = new Date(document.lastModified);
+document.getElementById("modificacao").textContent = dataMod.toLocaleDateString('pt-BR') + " " + dataMod.toLocaleTimeString('pt-BR');
+
+// 3. Cálculo da Sensação Térmica
+const temp = 9;   // Temperatura fixa
+const vento = 10; // Vento fixo
 
 function calcularSensacaoTermica(t, v) {
-
     if (t <= 10 && v > 4.8) {
         let sensacao = 13.12 + (0.6215 * t) - (11.37 * Math.pow(v, 0.16)) + (0.3965 * t * Math.pow(v, 0.16));
         return Math.round(sensacao) + " °C";
@@ -15,4 +18,5 @@ function calcularSensacaoTermica(t, v) {
     }
 }
 
+// Executa e insere na tela
 document.getElementById("sensacao").textContent = calcularSensacaoTermica(temp, vento);
